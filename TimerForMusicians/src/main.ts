a5966194e219899ccbd725a1289806ac2430b93f
Hidde Kramer <hidde@source-elements.com>
2023-03-05 23:45:17 +0100

    no websocket anymore

--- TimerForMusicians/src/main.ts
+++ TimerForMusicians/src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { WebSocketServer } from 'ws'
 import './assets/main.css'
-
 
 const app = createApp(App)
 
